a576d953551ac1ae6d358348037d72deafbd55c2
markbumpy <markbumpy91@gmail.com>
2024-05-20 19:02:30 +0100

    made all the file uploads in to one view and changed the urls, deleted the other html files

--- RagApp/HS/urls.py
+++ RagApp/HS/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import QueryPDF, PDFView, XLSXView, QueryXLSX, DOCXView, QueryDocx, CSVView, QueryCSV, DatabaseConnectionView, QueryDatabaseView
+from .views import QueryFile, UploadView, DatabaseConnectionView, QueryDatabaseView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -10,24 +10,13 @@ urlpatterns = [
     # path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 
     # # Urls for PDF upload and querying
-    path('querypdf_view/', QueryPDF.as_view(), name='querypdf_view'),
-    path('uploadpdf_view/', PDFView.as_view(), name="pdf_view"),
-
-
-    path('querycsv_view/', QueryCSV.as_view(), name='querycsv_view'),
-    path('uploadcsv_view/', CSVView.as_view(), name="csv_view"),
+    path('queryfile_view/', QueryFile.as_view(), name='queryfile_view'),
+    path('chatwfile/', UploadView.as_view(), name="uploadfile_view"),
     
-    # Urls for Excel files and querying
-    path('uploadxlsx_view/', XLSXView.as_view(), name="xlsx_upload"),
-    path('queryxlsx_view/', QueryXLSX.as_view(), name="xlsx_query"),
+    # # Urls for Excel files and querying
+ 
 
-
-    path('uploaddocx_view/', DOCXView.as_view(), name="docx_upload"),
-    path('querydocx_view/', QueryDocx.as_view(), name="docx_query"),
-
-
-
-    path('connect1/', DatabaseConnectionView.as_view(), name='db_connect'),
+    path('askdatabase/', DatabaseConnectionView.as_view(), name='db_connect'),
     path('query/', QueryDatabaseView.as_view(), name='db_query'),
 
     # path('query/', QueryDBView.as_view(), name='db_query'),
