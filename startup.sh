0a3f8b34ab0af036086f8f470784e039487d6922
markbumpy <markbumpy91@gmail.com>
2024-06-27 12:16:00 +0100

    changed pyhton path

--- startup.sh
+++ startup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Navigate to the directory containing manage.py
-cd $(dirname "$0")
+# Set the Python path to include your project root
+export PYTHONPATH=$PYTHONPATH:/home/site/wwwroot/PG_RAG
+
 
 # Run migrations (optional, but often useful)
-python manage.py migrate
+cd /home/site/wwwroot/PG_RAG/RagApp
+
 
 # Start Gunicorn server
-exec gunicorn --workers 3 --bind=0.0.0.0:8000 RagApp.RagApp.wsgi:application
+exec gunicorn --workers 3 --bind=0.0.0.0:8000 RagApp.wsgi:application
