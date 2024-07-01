6ab7afeab177260b974e3a9399cb2bcb5ef3cefb
markbumpy <markbumpy91@gmail.com>
2024-07-01 12:33:59 +0100

    changed path to wsgi module

--- startup.sh
+++ startup.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
 
-echo "Current directory: $(pwd)"
-echo "Python version: $(python --version)"
-
-# Set the Python path to include your project root
-export PYTHONPATH=$PYTHONPATH:/home/site/wwwroot/pg_RAG:/home/site/wwwroot/pg_RAG/RagApp
-echo "PYTHONPATH: $PYTHONPATH"
-
-
-
-# Navigate to the correct directory
-cd /home/site/wwwroot/RagApp || exit
-echo "Changed to directory: $(pwd)"
-
-
-# List files in current directory
-echo "Files in current directory:"
-ls -la
-
-
-# Start Gunicorn server
-echo "Starting Gunicorn..."
-exec gunicorn --workers 3 --bind=0.0.0.0:8000 RagApp.wsgi:application
+# <module-path> is the relative path to the folder that contains the module
+# that contains wsgi.py; <module> is the name of the folder containing wsgi.py.
+gunicorn --bind=0.0.0.0 --timeout 600 --workers=4 --chdir RagApp/RagApp RagApp.wsgi
