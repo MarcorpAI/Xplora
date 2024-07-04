4c96d6344b4117c4bd65853a2379ce63856cd028
markbumpy <markbumpy91@gmail.com>
2024-07-05 00:17:51 +0100

    still deployin..

--- RagApp/RagApp/settings.py
+++ RagApp/RagApp/settings.py
@@ -222,7 +222,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'HS/' 'static_in_env')]
 VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
 
