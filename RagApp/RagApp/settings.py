871b61f4f6af1d7f36861b1b3488d06e1b3c99aa
markbumpy <markbumpy91@gmail.com>
2024-07-05 15:07:27 +0100

    changed static folder

--- RagApp/RagApp/settings.py
+++ RagApp/RagApp/settings.py
@@ -220,9 +220,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'HS/' 'static_in_env')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 VENV_PATH = os.path.dirname(BASE_DIR)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
-
--- RagApp/RagApp/wsgi.py
+++ RagApp/RagApp/wsgi.py
@@ -1,17 +1,9 @@
 import os
+from django.core.wsgi import get_wsgi_application
 
-import sys
-
-project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-ragapp_dir = os.path.join(project_root, 'RagApp')
-sys.path.append(project_root)
-sys.path.append(ragapp_dir)
 
 
 settings_module = 'RagApp.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'RagApp.settings'
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
